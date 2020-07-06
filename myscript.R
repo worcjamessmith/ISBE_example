cc736c5386f40f389d49de45c1189191fc602cbe
James Smith <worcjamessmith@gmail.com>
2020-07-06 08:31:03 -0400

    First commit

---
+++ myscript.R
@@ -0,0 +1,8 @@
+mydata <- iris
+
+plot(mydata$Sepal.Length, mydata$Sepal.Width)
+
+
+# https://github.com/MalikaIhle/ISBE_Symposium/blob/master/analysis_start.md
+# here the example data link doesn't work
+# can I create an issue or something?
